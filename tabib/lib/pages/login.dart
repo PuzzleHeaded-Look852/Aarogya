013b566208f925dc75595cc82788990539dd5060
PuzzleHeaded-Look852 <sanjuonpc@gmail.com>
2025-08-15 13:14:49 +0530

    Update login.dart

--- tabib/lib/pages/login.dart
+++ tabib/lib/pages/login.dart
@@ -233,7 +233,7 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
                                 ),
                           ),
                           Text(
-                            "Tabib",
+                            "Aarogya",
                             style: GoogleFonts.inter(
                               fontSize: 32,
                               fontWeight: FontWeight.w800,
